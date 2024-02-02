a9b03e68752a3a9a654aa1331192e91cfe3acdc3
Lyoshka <86190362+Lyoshkaa@users.noreply.github.com>
2024-02-02 18:33:36 +0300

    Update LeagueQaccept.py

--- LeagueQaccept.py
+++ LeagueQaccept.py
@@ -7,19 +7,21 @@ accept = '\accept.jpg'
 pytesseract.pytesseract.tesseract_cmd = \
     r'C:\Users\iunth\AppData\Local\Programs\Tesseract-OCR\tesseract'
 
-acceptImage = pyautogui.screenshot(region=(599,587,733,198))
-acceptWord = pytesseract.image_to_string(acceptImage)
-#findmatch = pyautogui.locateOnScreen('findMatch.png',confidence=0.5)
-#pyautogui.click(findmatch)
-#Bigger photo for locating queue pop        Box(left=599, top=587, width=733, height=198)
+acceptImage = pyautogui.screenshot(region=(599,587,733,198)) #Takes a picture of where would the queue pop appear (Only works when league is at full resolution and is in the middle of the screen).
+acceptWord = pytesseract.image_to_string(acceptImage) #Converts the previous picture to text to locate where the ACCEPT! button would be.
+print("Waiting for queue pop...")
+
 while acceptWord != 'ACCEPT!':
+    #while loop to keep looking constantly for the queue up, by the method used in line 10,11
     acceptImage = pyautogui.screenshot(region=(599,587,733,198))
     acceptWord = pytesseract.image_to_string(acceptImage)
-    print("Waiting for queue pop...")
-    #print(acceptWord)
+    
     if 'ACCEPT!' in acceptWord:
+        #acceptWord Prints out a lot of random text, but the word ACCEPT! is somewhere there, so it looks for the substring ACCEPT! there.
+        
         accept = pyautogui.locateOnScreen(accept,confidence=0.8)
-        print(acceptWord)
+        #Locates where the ACCEPT! button is, provided the picture of what it looks like for the program to find.
+        
         pyautogui.moveTo(accept)
         pyautogui.click()
         break
